bb916f80115b3d7f7043b9f4260b17928712fd2c
liguo ma <maliguo@outlook.com>
2025-05-11 21:58:08 +0800

    update Github pages

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/sdszk-redesign/" : "/",
 };
+// This configuration file sets the public path for the Vue.js application based on the environment.
