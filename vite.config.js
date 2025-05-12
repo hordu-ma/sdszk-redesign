d65e61cdf4d9ae0d92e661f6a9f2b7df98a64c4b
liguo ma <maliguo@outlook.com>
2025-05-12 09:35:22 +0800

    update config of Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/sdszk-redesign/",
+  base: "./",
   plugins: [vue()],
   resolve: {
     alias: {
@@ -12,16 +12,32 @@ export default defineConfig({
     },
   },
   build: {
-    assetsDir: "assets",
+    cssCodeSplit: false,
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
       output: {
+        manualChunks: {
+          "element-plus": ["element-plus"],
+          "vue-vendor": ["vue", "vue-router", "pinia"],
+        },
         chunkFileNames: "js/[name]-[hash].js",
         entryFileNames: "js/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash][extname]",
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split(".");
+          let extType = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          } else if (/mp4|webm/i.test(extType)) {
+            extType = "video";
+          }
+          return `${extType}/[name]-[hash][extname]`;
+        },
       },
     },
+  },
+  server: {
+    cors: true,
+    port: 3000,
+    strictPort: true,
+    origin: "http://localhost:3000",
   },
 });
