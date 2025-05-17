885d45735aeb233e3b90a0fd9a07a466672f0e98
liguo ma <maliguo@outlook.com>
2025-05-17 16:32:08 +0800

    Updates

---
+++ Resources.CFs6luY3.js
@@ -0,0 +1,2 @@
+import{_ as s}from"./index.DMm08F27.js";import{e,g as a,f as o}from"./vendor.BEMe8GcW.js";const r={class:"resources-container"},n=s({__name:"Resources",setup:s=>(s,n)=>(o(),e("div",r,n[0]||(n[0]=[a("h1",null,"资源中心",-1),a("div",{class:"resources-content"},[a("p",null,"资源中心内容正在建设中...")],-1)])))},[["__scopeId","data-v-a4afbda3"]]);export{n as default};
+//# sourceMappingURL=Resources.CFs6luY3.js.map
