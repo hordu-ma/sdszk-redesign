eb6a5eaa1c4c9079ff32da97f71b6bb266f114b9
liguo ma <maliguo@outlook.com>
2025-05-24 11:18:15 +0800

    Updates

--- Activities.Cpg7gtmm.js
+++
@@ -1,2 +0,0 @@
-import{_ as s}from"./index.CDT4yV2-.js";import{l as t,p as a,m as i}from"./vendor.BN0yMOrj.js";const e={class:"activities-container"},n=s({__name:"Activities",setup:s=>(s,n)=>(i(),t("div",e,n[0]||(n[0]=[a("h1",null,"活动中心",-1),a("div",{class:"activities-content"},[a("p",null,"活动中心内容正在建设中...")],-1)])))},[["__scopeId","data-v-4a4e1d23"]]);export{n as default};
-//# sourceMappingURL=Activities.Cpg7gtmm.js.map
