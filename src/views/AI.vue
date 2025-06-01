f1422cddefde4bc4bdc57e846e58b3bb603c61d7
liguo ma <maliguo@outlook.com>
2025-06-01 10:35:47 +0800

    update the AI.vue component

--- src/views/AI.vue
+++ src/views/AI.vue
@@ -3,7 +3,16 @@
     <h1>AI思政</h1>
     <div class="ai-content">
       <!-- AI思政内容将在这里展示 -->
-      <p>AI思政功能正在建设中...</p>
+      <p>AI思政功能正在建设中,简单示例如下...</p>
+    </div>
+    <div>
+      <iframe
+        src="http://localhost/chatbot/iHoXtxvZtBavk23g"
+        style="width: 100%; height: 100%; min-height: 700px"
+        frameborder="0"
+        allow="microphone"
+      >
+      </iframe>
     </div>
   </div>
 </template>
